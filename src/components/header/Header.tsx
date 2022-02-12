3dc4890483a20ce72b6dcfd2604a3bb17dedcea1
Nico Kandut <nico.kandut@gmail.com>
2022-02-12 18:03:45 +0100

    feat: social media ready

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,39 @@
 import { combine } from "../../logic/classnames";
 import { getUpdateAvailable } from "../../logic/update-state";
+import { Button } from "../button/Button";
+import GithubIcon from "../icon/GithubIcon";
 import "./Header.css";
 
 const Header = ({ className }: { className?: string }) => {
   return (
     <header className={combine("app-header", className)}>
       <h1 className="title">Factorio Solar Art Generator</h1>
-      {getUpdateAvailable() && (
+      {getUpdateAvailable() ? (
         <span
           className="header-update"
           onClick={() => window.location.reload()}
         >
           New version available. Click here to reload.
         </span>
+      ) : (
+        <span className="header-update"></span>
       )}
+      <Button
+        onClick={() => {
+          window.location.href =
+            "https://github.com/NicoKandut/factorio-solar-art";
+        }}
+      >
+        <GithubIcon />
+      </Button>
+      <Button
+        onClick={() => {
+          window.location.href =
+            "https://discordapp.com/users/272673243083571202";
+        }}
+      >
+        <span className="material-icons">discord</span>
+      </Button>
     </header>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,14 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { onUpdate } from "./logic/update-state";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://a6f44d458daf4987900f5bb9b04222fc@o1142273.ingest.sentry.io/6201203",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
